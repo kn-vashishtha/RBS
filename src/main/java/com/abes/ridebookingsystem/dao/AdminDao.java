157d51744d1f387d544c4eaeda42805ed33d8905
Anamika Verma <anamikaverma1412@gmail.com>
2025-05-30 15:58:42 +0530

    Admin dao by Anamika

--- src/main/java/com/abes/ridebookingsystem/dao/AdminDao.java
+++ src/main/java/com/abes/ridebookingsystem/dao/AdminDao.java
@@ -1,4 +1,16 @@
-package com.abes.ridebookingsystem.dao;
+package com.abes.ridebookingsystem.dao;                  // Package declaration 
 
-public class AdminDao {
+import com.abes.ridebookingsystem.dto.Admin;             // Importing the Admin class from the dto
+import java.util.Map;                                   // Importing Map interface
+
+public interface AdminDao {                              // Declaring the AdminDao interface
+	void addAdmin(Admin admin);                          // Method to add a new Admin
+
+	Admin getAdminById(String userId);                   // Method to retrieve a specific Admin(user Id as key)
+
+	void updateAdmin(Admin admin);                       // Method to update an existing Admin's information
+
+	void deleteAdmin(String userId);                     // Method to delete an Admin (user Id as Key)
+
+	Map<String, Admin> getAllAdmins();                   // Method to retrieve all Admins stored
 }
