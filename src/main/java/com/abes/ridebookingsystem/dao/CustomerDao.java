28f8b7b2a1b0292579702c7b138a7d6d76ee32e2
Saloni <saloni371410@gmail.com>
2025-05-30 16:00:47 +0530

    customer dao by saloni

--- src/main/java/com/abes/ridebookingsystem/dao/CustomerDao.java
+++ src/main/java/com/abes/ridebookingsystem/dao/CustomerDao.java
@@ -1,4 +1,25 @@
 package com.abes.ridebookingsystem.dao;
 
-public class CustomerDao {
+import com.abes.ridebookingsystem.dto.Customer;
+
+import java.util.Map;
+
+
+// interface related to customer operations
+
+public interface CustomerDao {
+	// add a customer
+	void addCustomer(Customer customer);
+	
+	// retrieve a customer based on their id
+	Customer getCustomerById(String userId);
+	
+	// updates the information of customer
+	void updateCustomer(Customer customer);
+	
+	//deletes a customer
+	void deleteCustomer(String userId);
+	
+	// retrieves all customers
+	Map<String, Customer> getAllCustomers();
 }
