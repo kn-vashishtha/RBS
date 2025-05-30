4d7346a0727319246e20724d7d1e1c93b08dac4b
RachitGarg-02 <gargrachit68@gmail.com>
2025-05-30 15:32:34 +0530

    Ride Exception uploaded by rachit

--- src/main/java/com/abes/ridebookingsystem/exception/RideException.java
+++ src/main/java/com/abes/ridebookingsystem/exception/RideException.java
@@ -1,4 +1,15 @@
 package com.abes.ridebookingsystem.exception;
 
-public class RideException {
+public class RideException extends Exception {
+	public RideException() {
+		super();
+	}
+
+	public RideException(String message) {
+		super(message);
+	}
+
+	public RideException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
