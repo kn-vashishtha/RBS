72faa5893daf11fe85c326325346acc099d2e4a9
6SHREYA <shreya06lg@gmail.com>
2025-05-30 15:58:18 +0530

    User dao impl exceptions and util

---
+++ src/main/java/com/abes/ridebookingsystem/util/DataCollection.java
@@ -0,0 +1,11 @@
+package com.abes.ridebookingsystem.util;
+
+import com.abes.ridebookingsystem.dto.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataCollection {
+
+	public static final Map<String, User> userMap = new HashMap<>();
+}
