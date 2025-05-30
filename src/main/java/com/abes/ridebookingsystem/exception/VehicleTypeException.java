8d58ceae524d507dc027c4a46501b7702f002e05
Keshav Narayana Vashishtha <kn.vashishtha22@gmail.com>
2025-05-30 15:50:34 +0530

    vehicle exception by keshav

--- src/main/java/com/abes/ridebookingsystem/exception/VehicleTypeException.java
+++ src/main/java/com/abes/ridebookingsystem/exception/VehicleTypeException.java
@@ -1,4 +1,15 @@
 package com.abes.ridebookingsystem.exception;
 
-public class VehicleTypeException {
+public class VehicleTypeException extends Exception {
+	public VehicleTypeException() {
+		super();
+	}
+
+	public VehicleTypeException(String message) {
+		super(message);
+	}
+
+	public VehicleTypeException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
