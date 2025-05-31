20d965e72ab6b3eb9dde08c8f9d5b3c3f2a2b0ef
Saloni <saloni371410@gmail.com>
2025-05-31 14:22:55 +0530

    Customer service by saloni

--- src/main/java/com/abes/ridebookingsystem/service/CustomerService.java
+++ src/main/java/com/abes/ridebookingsystem/service/CustomerService.java
@@ -1,4 +1,23 @@
 package com.abes.ridebookingsystem.service;
 
-public class CustomerService {
+import com.abes.ridebookingsystem.dto.Customer;
+import java.util.Map;
+
+// This interface defines what operations can be done for a customer
+public interface CustomerService {
+
+    // Add a new customer to the system
+    void addCustomer(Customer customer);
+
+    // Get a customer's details using their user ID
+    Customer getCustomerById(String userId);
+
+    // Update the details of an existing customer
+    void updateCustomer(Customer customer);
+
+    // Remove a customer from the system using their user ID
+    void deleteCustomer(String userId);
+
+    // Get a list of all customers
+    Map<String, Customer> getAllCustomers();
 }
