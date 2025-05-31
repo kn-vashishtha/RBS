525d2b49b68724f1433545caf89900530726dd6a
Anamika Verma <anamikaverma1412@gmail.com>
2025-05-31 14:26:54 +0530

    AdminService and AdminServiceImpl by anamika

--- src/main/java/com/abes/ridebookingsystem/service/AdminService.java
+++ src/main/java/com/abes/ridebookingsystem/service/AdminService.java
@@ -1,4 +1,13 @@
 package com.abes.ridebookingsystem.service;
 
-public class AdminService {
+import com.abes.ridebookingsystem.dto.Admin;
+import java.util.Map;
+
+public interface AdminService {
+    void addAdmin(Admin admin);             // Adds a new Admin to the system
+    Admin getAdminById(String userId);       //Retrieves an Admin by their user ID
+    void updateAdmin(Admin admin);          // Updates an existing Admin's details
+    void deleteAdmin(String userId);        //Deletes an Admin from the system by their user ID.
+    Map<String, Admin> getAllAdmins();      // Retrieves all Admins in the system
 }
+
